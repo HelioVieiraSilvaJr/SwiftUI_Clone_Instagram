a6b2bf432b18398948df4c956d8f68798161c826
Helio Vieira <helio.engenharia@gmail.com>
2022-09-14 12:32:53 -0300

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Helio Junior on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
