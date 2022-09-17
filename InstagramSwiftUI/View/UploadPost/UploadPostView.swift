92298c57192e3f64e5281bda9edc351ae0311dd7
Helio Vieira <helio.engenharia@gmail.com>
2022-09-17 15:36:45 -0300

    ⭐️: Created UploadPost View and same things.

--- InstagramSwiftUI/View/UploadPost/UploadPostView.swift
+++ InstagramSwiftUI/View/UploadPost/UploadPostView.swift
@@ -8,10 +8,69 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    @State var imagePickerPresented = false
+    
     var body: some View {
-        Text("UploadPost")
+        VStack {
+            if postImage == nil {
+                Button {
+                    imagePickerPresented.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .foregroundColor(Color.black)
+                        .overlay {
+                            Circle()
+                                .stroke(Color.black, lineWidth: 1)
+                        }
+                }
+                .padding(.top, 56)
+                .sheet(isPresented: $imagePickerPresented) {
+                    guard let image = selectedImage else { return }
+                    postImage = Image(uiImage: image)
+                } content: {
+                    ImagePicker(image: $selectedImage)
+                }
+
+            }
+            else if let postImage = postImage {
+                HStack(alignment: .top) {
+                    postImage
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                        .foregroundColor(Color.black)
+                    
+                    TextField("Escreva seu comentário...", text: $captionText)
+                }
+                .padding()
+                
+                Button {
+                    
+                } label: {
+                    Text("Compartilhar")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                }
+
+            }
+            
+            Spacer()
+        }
     }
 }
+
 
 struct UploadPostView_Previews: PreviewProvider {
     static var previews: some View {
